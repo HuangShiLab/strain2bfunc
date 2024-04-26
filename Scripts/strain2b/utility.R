20915e840bce005359a6bec256ad91ccae320d39
yfz-96 <qdu_zyf@163.com>
2024-04-26 14:30:59 +0800

    Update files

--- Scripts/strain2b/utility.R
+++ Scripts/strain2b/utility.R
@@ -51,17 +51,17 @@ Merge_Copynumber_Matrix <- function(all_species, cnm_matrix_dir) { # merge the c
   }
   
   rownames(result) <- result[, 1]
-  
+  result <- result[ , -1, drop = F]
   return (result)
 }
 
 Rename_sample <- function(sample_name) {
 # Since sequence names for vsearch need to include file names, 
-# but the file names cannot contain "." and "-", 
-# the "." and "-" in file names need to be replaced with "_"
+# but the file names cannot contain ".", 
+# the "." in file names need to be replaced with "_"
   sample_name <- gsub("\\.", "_", sample_name)
   sample_name <- gsub("-", "_", sample_name)
- 
+
   return (sample_name)
 }
 
@@ -93,17 +93,127 @@ Vsearch <- function(cnm, new_sample_fa, similarity, output_tag_path, tags_count_
   
   system(cmd, intern = TRUE)
   
-  file.remove(new_sample_fa) #Delete intermediate result files
-  file.remove(output_tag_path) #Delete intermediate result files
+  #file.remove(new_sample_fa) #Delete intermediate result files
+  #file.remove(output_tag_path) #Delete intermediate result files
 }
 
+corrDist <- function(X){
+  csd <- apply(X, 2, sd)
+  if(sum(csd == 0) > 0){
+    stop("Columns", which(csd == 0), "have no variance! Cannot compute correlation distances")
+  }
+  CSD <- csd %*% t(csd)
+  cm <- colMeans(X)
+  CM <- cm %*% t(cm)
+  n <- nrow(X)
+  XX <- crossprod(X)
+  D <- 1 - (as.matrix(XX) - n * CM) / ((n - 1) * CSD)
+  return(D)
+}
+
+#' @name deduplicate_cnm
+#' 
+#' @description Check the auto-correlation between columns, and deduplicate the columns correlated with any others from the copy number matrix.
+#'
+#' @param copy_number_matrix A copy-number matrix (Matrix) with no columns has a variance of 0.
+#'
+#' @return An updated copy-number matrix with columns that are all uncorrelated.
+#'
+#' @author 
+#'
+#' @importFrom Matrix crossprod colMeans
+#' @example
+#' cnm <- matrix(c(1, 1, 2, 2, 1,
+#'                 1, 1, 2, 2, 1,
+#'                 1, 1, 5, 5, 1,
+#'                 2, 2, 4, 4, 2),
+#'               nrow = 4, byrow = T)
+#' colnames(cnm) <- paste0("g_", 1:5)
+#' new_cnm <- deduplicate_cnm(cnm, threshold = 0.2)
+
+deduplicate_cnm <- function(copy_number_matrix, threshold = 0.2) {
+  csd <- apply(copy_number_matrix, 2, sd)
+  if(sum(csd == 0) > 0){
+    stop("Columns", which(csd == 0), "have no variance! Cannot compute correlation distances")
+  }
+  cor_dist <- corrDist(copy_number_matrix)
+  index <- which(cor_dist < threshold, arr.ind = TRUE)
+  index <- index[index[, 1] != index[, 2], ]
+  new_index <- index
+  new_index[, 1] <- apply(index, 1, function(x) {min(x)})
+  new_index[, 2] <- apply(index, 1, function(x) {max(x)})
+  delete_index <- unique(new_index[, 1])
+  copy_number_matrix <- copy_number_matrix[, -delete_index]
+  return(copy_number_matrix)  
+}
+
+#' @name check_corr_in_cnm
+#' 
+#' @description Check the auto-correlation between columns, and deduplicate the columns correlated with any others from the copy number matrix.
+#'
+#' @param copy_number_matrix A copy-number matrix (Matrix).
+#'
+#' @return An updated copy-number matrix with columns that are all uncorrelated.
+#'
+#' @author 
+#'
+#' @importFrom Matrix crossprod colMeans
+#' @example
+#' cnm <- matrix(c(0, 1, 2, 1, 1, 2, 2, 1,
+#'                 0, 1, 2, 1, 1, 2, 2, 1,
+#'                 0, 1, 2, 1, 1, 5, 5, 1,
+#'                 0, 1, 2, 2, 2, 4, 4, 2),
+#'               nrow = 4, byrow = T)
+#' colnames(cnm) <- paste0("g_", 1:8)
+#' new_cnm <- check_corr_in_cnm(cnm, threshold = 0.2)
+#' 
+
+check_corr_in_cnm <- function(copy_number_matrix, threshold = 0.2) {
+  copy_number_matrix <- as.matrix(copy_number_matrix)
+  csum <- colSums(copy_number_matrix)
+  
+  new_copy_number_matrix <- copy_number_matrix[, which(csum != 0)]
+  
+  csd <- apply(new_copy_number_matrix, 2, sd)
+  
+  #If there are no columns with zero variance, 
+  #then directly deduplicate the columns correlated with any others from the copy number matrix
+  if(sum(csd == 0) == 0) {
+    new_copy_number_matrix <- deduplicate_cnm(new_copy_number_matrix, threshold)
+  } 
+  #If there is one column with zero variance, then retain the column,
+  #and then deduplicate the columns correlated with any others from the retained copy number matrix
+  else if(sum(csd == 0) == 1) {
+    csd_0 <- which(csd == 0)
+    col_csd_0 <- new_copy_number_matrix[, csd_0, drop = F]
+    new_copy_number_matrix <- new_copy_number_matrix[, -csd_0, drop = F]
+    new_copy_number_matrix <- deduplicate_cnm(new_copy_number_matrix, threshold)
+    new_copy_number_matrix <- data.frame(col_csd_0, new_copy_number_matrix)
+  } 
+  #If there are more than one columns with zero variance, then retain one of the columns, 
+  #and then deduplicate the columns correlated with any others from the retained copy number matrix
+  else { #sum(csd == 0) > 1
+    csd_0 <- which(csd == 0)
+    col_csd_0 <- new_copy_number_matrix[, csd_0[1], drop = F]
+    new_copy_number_matrix <- new_copy_number_matrix[, -csd_0, drop = F]
+    new_copy_number_matrix <- deduplicate_cnm(new_copy_number_matrix, threshold)
+    new_copy_number_matrix <- data.frame(col_csd_0, new_copy_number_matrix)
+  }
+  
+  cat(ncol(copy_number_matrix) - ncol(new_copy_number_matrix), " genomes have been removed from the copy number matrix.\n")
+  removed_genomes <- colnames(copy_number_matrix)[! colnames(copy_number_matrix) %in% colnames(new_copy_number_matrix)]
+  cat("The removed genomes: ", removed_genomes, "\n")
+  cat(ncol(new_copy_number_matrix), " genomes were retained in the copy number matrix.\n")
+  
+  return (new_copy_number_matrix)
+}
 
 Filter_CNM <- function(cnm, tags_count_file) {
 #filter the copynumber matrix according to the vsearch result (delete the tags which are not included in the sample)
 
   tags_count <- read.table(tags_count_file, sep = "\t", header = T, row.names = 1, comment="")
 
-  file.remove(tags_count_file) #Delete intermediate result files
+  #file.remove(tags_count_file) #Delete intermediate result files
   
   idx <- rownames(tags_count) %in% rownames(cnm)
   tags_count <- subset(tags_count, idx)
@@ -111,8 +221,14 @@ Filter_CNM <- function(cnm, tags_count_file) {
   idx <- rownames(cnm) %in% rownames(tags_count)
   cnm <- subset(cnm, idx)
 
-  cnm <- unique(cnm, MARGIN=2)
-  cnm <- cnm[, -1] # The first column of the copy number matrix is the tag names
+  cnm <- cnm[, !duplicated(as.list(as.data.frame(cnm))), drop = F]
+  
+  # cnm <- unique(cnm, MARGIN=2)
+  # cnm <- cnm[, -1] # The first column of the copy number matrix is the tag names
+
+  cnm <- cnm[, which(colSums(cnm) > 0), drop = F]
+  
+  cnm <- check_corr_in_cnm(cnm)
 
   tags_count <- tags_count[order(rownames(tags_count)), , drop = FALSE]
   cnm <- cnm[order(rownames(cnm)), , drop = FALSE]
@@ -196,6 +312,8 @@ One_Sample_Pipeline <- function(sample_info, species_info, output_path, mode, cn
 
   tags_count <- matrix$tags_count
   cnm <- matrix$cnm
+  write.table(tags_count, paste0(output_path, "/filtered_", sample_name, "_reads_count_matrix.txt"), sep = "\t", quote = F, row.names = T, col.names = NA)
+  write.table(cnm, paste0(output_path, "/filtered_", sample_name, "_copy_number_matrix.txt"), sep = "\t", quote = F, row.names = T, col.names = NA)
 
   strain_abd_matrix <- Strain_Level_Profiling(tags_count, cnm)
 
@@ -283,11 +401,13 @@ Sample_List_Pipeline <- function(sample_list_file, species_list_file, output_pat
   }
 
   profile_list <- apply(sample_list, 1, function(x) tryCatch({One_Sample_Pipeline(x, species_list, output_path, mode, cnm_matrix_dir, cnm)}, error=function(err) { NA }))
+  
+  # print(profile_list)
 
   profile_list <- do.call(rbind, profile_list)
  
   # print(profile_list)
-  # write.table(profile_list, paste0(output_path, "/tmp_profile_list.txt"), sep = "\t", quote = F, row.names = T, col.names = NA) 
+  write.table(profile_list, paste0(output_path, "/tmp_profile_list.txt"), sep = "\t", quote = F, row.names = T, col.names = NA) 
 
   sample_name_list <- sample_list[, 1]
   abd_matrix <- Merge_Profiling_Matrix(profile_list, sample_name_list)
